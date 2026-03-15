9ae485b8baaa7b43778c8969c5007652583852e8
Aman Verma <amanverma.codes@gmail.com>
2026-03-16 01:30:03 +0530

    cleanup

--- .next/dev/static/chunks/src_app_layout_1cf6b850.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__529b3409._.css",
-  "static/chunks/node_modules_cf4645a1._.js"
-],
-    source: "dynamic"
-});
